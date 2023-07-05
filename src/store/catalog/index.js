cfcf9fbcb28cf90147b3de155ca567c170ecb4a3
Andrey Bulavskiy <10vvvvvv31@gmail.com>
2023-07-05 21:33:34 +0300

    add comment in vuex action catalog/addProductInCatalog

--- src/store/catalog/index.js
+++ src/store/catalog/index.js
@@ -6,6 +6,14 @@ export default {
   getters: {},
   mutations: {},
   actions: {
+    /**
+     * @param {Object} state
+     * @param {
+     *    id: Number,
+     *    price: Number,
+     *    count: Number
+     * } productObject price object
+     */
     addProductInCatalog: (state, productObject) => {
       state.dispatch("catalog/basket/addProduct", productObject);
       state.dispatch("catalog/products/addProductElement", productObject);
